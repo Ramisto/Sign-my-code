f5fec22440bb64b2619ac5c5d6cb9d2aca166109
Ramisto <Ramisto@protonmail.com>
2023-10-05 21:20:36 +0200

    first lines

--- sign-my-code.sh
+++ sign-my-code.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+# install-gpg () {
+#    if [ $(dpkg-query -W -f='${Status}' gpg 2>/dev/null | grep -c "ok installed") -eq 0 ];
+#    then
+#    sudo apt-get install gpg;
+#    fi
+# }
 
 
+# install-gpg
