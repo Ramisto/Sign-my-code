8ef960a78c05632a6d2e100991852dae1f6b9d15
Ramisto <Ramisto@protonmail.com>
2023-10-06 08:47:48 +0200

    install gpg function

--- sign-my-code.sh
+++ sign-my-code.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 
-# install-gpg () {
-#    if [ $(dpkg-query -W -f='${Status}' gpg 2>/dev/null | grep -c "ok installed") -eq 0 ];
-#    then
-#    sudo apt-get install gpg;
-#    fi
-# }
+ install-gpg () {
+    if [ $(dpkg-query -W -f='${Status}' gpg 2>/dev/null | grep -c "ok gpg installed") -eq 0 ];
+    then
+        echo -e '\031[A;B;Cm gpg not installed \033[0m'
+        apt install gpg;
+    fi
+}
 
 
-# install-gpg
+install-gpg
