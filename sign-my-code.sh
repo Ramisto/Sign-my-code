29854d703d4e72b9cbe1067c09d61e348d0f5126
Ramisto <Ramisto@protonmail.com>
2023-10-07 10:46:44 +0200

    update banner, packages, check gpg and update select menu

--- sign-my-code.sh
+++ sign-my-code.sh
@@ -1,74 +1,54 @@
 #!/usr/bin/bash
 
 # ----------------------------------
-# Colors
-# ----------------------------------
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
-LIGHTPURPLE='\033[1;35m'
-LIGHTCYAN='\033[1;36m'
-WHITE='\033[1;37m'
-
-
-# ----------------------------------
 # Functions
 # ----------------------------------
 
 banner () {
-echo "     _______. __    _______ .__   __.    .___  ___. ____    ____      ______   ______    _______   _______ ";
-echo "    /       ||  |  /  _____||  \ |  |    |   \/   | \   \  /   /     /      | /  __  \  |       \ |   ____|";
-echo "   |   (----\|  | |  |  __ |\|   |  |    \  /  |  \  \/   /   |    ,----'|  | | |  |  .--.  ||   ||__   ";
-echo "    \   \    |  | |  | |_ | |  .\`  |    |  |\/|  |   \_    _/      |  |    | | |  |  | |  |  |  ||   __|  ";
-echo ".----)   |   |  | |  |__| | |  |\   |    |  |  |  |     |  |        |  \`----.|  \`--'| |  '--'  ||____ ";
-echo "|_______/    |__|  \______| |__| \__|    |__|  |__|     |__|         \______| \______/  |_______/ |_______|";
-echo "                                                                                                           ";
+echo -e "\033[0;35m     _______. __    _______ .__   __.    .___  ___. ____    ____      ______   ______    _______   _______ \033[0m";
+echo -e "\033[0;35m    /       ||  |  /  _____||  \ |  |    |   \/   | \   \  /   /     /      | /  __  \  |       \ |   ____|\033[0m";
+echo -e "\033[0;35m   |   (----\|  | |  |  __|\|  |    |    \  /  |  \  \/   /   |    ,----'|  | | |  |  .--.  ||   ||__   \033[0m";
+echo -e "\033[0;35m    \   \    |  | |  | |_ | | .\`   |     |  |\/|  |   \_    _/      |    |    | |  |  | | |  |  |||   __|  \033[0m";
+echo -e "\033[0;35m.----)   |   |  | |  |__| | |  |\   |    |  |  |  |     |  |        |    \`--. |  \`--' | |  '--'  ||____ \033[0m";
+echo -e "\033[0;35m|_______/    |__|  \______| |__| \__|    |__|  |__|     |__|         \______| \______/  |_______/ |_______|\033[0m";
+echo -e "";
+echo "";
+echo "";    
 }
 
+gpg-git-install () {
+    # Function for install gpg and git packages 
+    echo -e '\033[0;35m--- Installing GPG ---\033[0m';
+    apt -y install gpg
+    echo -e '\033[0;35m-----------------------\033[0m';
 
-gpg-install () {
-    # Verify if gpg is already installed, if not, install the package
-    pkg=gpg
-    status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
-    NOCOLOR='\033[0m'
-    RED='\033[0;31m'
-
-    if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-    echo -e '\033[0;31m### Installing GPG... ###\033[0m'
-    sudo apt -y install $pkg
-    fi
-}
-
-git-install () {
-    # Verify if git is already installed, if not, install the package
-    pkg=git
-    status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
-
-    if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-    echo -e '\033[0;31m### Installing GIT... ###\033[0m'
-    sudo apt -y install $pkg
-    fi
+    echo -e '\033[0;35m--- Installing Git ---\033[0m';
+    apt -y install git
+    echo -e '\033[0;35m-----------------------\033[0m';
 }
 
 check-GPG-key () {
-    # Checking for existing GPG keys
+    # Check for existing GPG keys
     # This command to list the long form of the GPG keys
-    echo -e '\033[0;31m### Checking for existing GPG keys... ###\033[0m'
+    echo -e '\033[0;35m--- Check for existing GPG keys ---\033[0m';
     gpg --list-secret-keys --keyid-format=long
-    echo -e '\033[0;31m########\033[0m'
+    echo -e '\033[0;35m-----------------------\033[0m';
 }
 
 generate-new-GPG-key () {
+    echo 'hello';
+}
+
+add-gpg-key-github () {
+    echo 'hello';
+}
+
+configure-git-cli () {
+    echo 'hello';
+}
+
+sign-commit-examples () {
+    echo 'hello';
 }
 
 
@@ -76,25 +56,37 @@ generate-new-GPG-key () {
 # Select menu
 # ----------------------------------
 
+banner
+
 PS3='Please enter your choice: '
+options=("Install gpg and git packages" "Check for existing GPG keys" "Generate a new GPG key" "Add a GPG key to your GitHub account" "Tell Git about your signing key" "Sign commits examples" "Quit")
 
-options=("Option 1" "Option 2" "Option 3" "Quit")
-
-select opt in "${options[@]}"
+select opt in "${options[@]}";
 do
     case $opt in
-        "Option 1")
-            echo "you chose choice 1"
+        "Install gpg and git packages")
+            gpg-git-install
             ;;
-        "Option 2")
-            echo "you chose choice 2"
+        "Check for existing GPG keys")
+            check-GPG-key
             ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
+        "Generate a new GPG key")
+            generate-new-GPG-key
+            ;;
+        "Add a GPG key to your GitHub account")
+            add-gpg-key-github
+            ;;
+        "Tell Git about your signing key")
+            configure-git-cli
+            ;;
+        "Sign commits examples")
+            sign-commit-examples
             ;;
         "Quit")
             break
             ;;
-        *) echo "invalid option $REPLY";;
+        *) 
+            echo "Invalid option $REPLY"
+            ;;
     esac
 done
